8da015656f0329ad6cce2155fed8c48ad194fe2d
Biebem <lammens.f96@gmail.com>
2021-01-28 17:10:05 +0100

    added channel message

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ const Twitter = require(`./Twitter`);
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
-
-client.on(`ready`, async (server) =>{
-   let response = await Twitter.getLatestTweet();
-
-   const channel = server.guild.channels.find(ch => ch.name === 'development');
-   channel.send(`The message :`);
-
+client.on(`ready`, async () =>{
+   const channel = client.channels.cache.find(channel => channel.name === "development");
+   let message = await Twitter.getLatestTweet();
+   channel.send("message");
 })
+
 
 client.on('message', msg => {
     if (msg.content === 'ping') {

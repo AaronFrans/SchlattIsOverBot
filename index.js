426223617cd860ba027dd18450f080aa379b983a
Biebem <lammens.f96@gmail.com>
2021-01-28 18:00:15 +0100

    added embed for message

--- index.js
+++ index.js
@@ -2,22 +2,26 @@ require(`dotenv`).config();
 const Twitter = require(`./Twitter`);
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const embed = require('discord-embed-maker');
 
-client.on(`ready`, async () => {
+
+const sendMessage = async () => {
+
   const channel = client.channels.cache.find(
     (channel) => channel.name === "development"
   );
   let message = await Twitter.getLatestTweet();
 
+  embed.setTitle(`@${process.env.HANDLE}`);
+  embed.setFooter(`${message.date.toISOString().replace(/T/," ").replace(/\..+/,"")}`);
+  embed.setDescription((message.value));
+  embed.setImage("https://static.wikia.nocookie.net/my-hero-academia-the-pinnacle/images/4/44/Jonathan_schlatt.jpg/revision/latest/scale-to-width-down/256?cb=20190814233055");
   console.log(message);
-  channel.send(
-    `@${
-      process.env.HANDLE
-    } just tweeted:\n ${message.date
-      .toISOString()
-      .replace(/T/, " ")
-      .replace(/\..+/, "")}\n ${message.value}`
-  );
+  channel.send({embed:embed});
+}
+
+client.on(`ready`, async () => {
+ sendMessage();
 });
 
 client.on("message", (msg) => {

5d0b9669ef9aa4189632b74349d99da4315f26f8
AaronFrans <azdefatrhtws@gmail.com>
2021-01-31 16:40:59 +0100

    Intro message on launch works

--- index.js
+++ index.js
@@ -5,12 +5,31 @@ const client = new Discord.Client();
 const embed = require("discord-embed-maker");
 
 let lastTweetDate = null;
-const prefix = '#';
+const prefix = "#";
 let timout = null;
 
-
 client.on(`ready`, () => {
+
+  
+  client.guilds.cache.map((guild) => sendIntroMessage(guild));
 });
+
+const sendIntroMessage = (guild) => {
+  let channelID;
+  let channels = guild.channels.cache;
+
+  channelLoop:
+  for (let key in channels) {
+      let c = channels[key];
+      if (c[1].type === "text") {
+          channelID = c[0];
+          break channelLoop;
+      }
+  }
+
+  let channel = guild.channels.cache.get(guild.systemChannelID || channelID);
+  channel.send(`Please set the channel the bot should post in via the \"#set\" command.`);
+};
 
 const sendNewest = async () => {
   const channel = client.channels.cache.find(
@@ -30,10 +49,11 @@ const sendNewest = async () => {
     embed.setImage(
       "https://static.wikia.nocookie.net/my-hero-academia-the-pinnacle/images/4/44/Jonathan_schlatt.jpg/revision/latest/scale-to-width-down/256?cb=20190814233055"
     );
+
     channel.send({ embed: embed });
   }
 
-  timout = setTimeout(sendNewest, 10000).;
+  timout = setTimeout(sendNewest, 10000);
 };
 
 client.on("message", (msg) => {
@@ -42,18 +62,17 @@ client.on("message", (msg) => {
   const args = msg.content.slice(prefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
 
-  switch(command) {
-    case 'start':
-      msg.channel.send('Started posting new tweets.');
+  switch (command) {
+    case "start":
+      msg.channel.send("Started posting new tweets.");
       sendNewest();
       break;
-    case 'stop':
-      msg.channel.send('Stopped posting new tweets.');
-      if(timout != null)
-       clearTimeout(timout);
+    case "stop":
+      msg.channel.send("Stopped posting new tweets.");
+      if (timout != null) clearTimeout(timout);
       break;
     default:
-      msg.channel.send('Not a valid command');
+      msg.channel.send("Not a valid command");
   }
 });
 
